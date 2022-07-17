7979dae997d655f2238b7586ab0e5dec54ee8791
Sunday Ezeilo <ezeilo.su@gmail.com>
2022-07-17 18:28:37 +0100

    Add docker script

---
+++ auth/src/index.ts
@@ -0,0 +1,14 @@
+import express, { json, urlencoded } from 'express';
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.get('api/users/currentuser', (req, res) => {
+  res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!');
+});
