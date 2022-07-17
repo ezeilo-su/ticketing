dfcedfdbf619d5e699472ca1cadaeaf42f7375d1
Sunday Ezeilo <ezeilo.su@gmail.com>
2022-07-17 20:36:55 +0100

    Add ingress service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,10 +5,10 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.get('api/users/currentuser', (req, res) => {
+app.get('/api/users/currentuser', (req, res) => {
   res.send('Hi there!');
 });
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000!!');
+  console.log('Listening on port 3000!');
 });
